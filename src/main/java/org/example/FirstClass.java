b2beb1677d001a1ce5623e049c54a01d8baf0a99
Shehan Perera <12shehanakalanka@gmail.com>
2024-08-01 20:50:11 +0530

    first commit

---
+++ src/main/java/org/example/FirstClass.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class FirstClass {
+    private String username;
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public String getUsername(){
+        return username;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       FirstClass firstClass = new FirstClass();
+       firstClass.setUsername("Shehan");
+        System.out.println(firstClass.getUsername());
+    }
+}
