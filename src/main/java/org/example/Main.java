07bc900f7943b9575962def6d5413ae1f446eec8
Shehan Perera <12shehanakalanka@gmail.com>
2024-08-07 20:19:35 +0530

    Add Conflicts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Encapsulation encapsulation = new Encapsulation();
-        encapsulation.setUsername("Shehan");
+        encapsulation.setUsername("Shehan Akalanka");
         System.out.println(encapsulation.getUsername());
     }
 }
